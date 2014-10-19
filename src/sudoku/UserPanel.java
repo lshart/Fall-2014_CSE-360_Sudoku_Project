381c325186079cec33a005c2ed685ef78d0cb833
Luke S. Hart <lshart@asu.edu>
2014-10-19 13:02:44 -0700

    Updated model, and an experiment with window builder

---
+++ src/sudoku/UserPanel.java
@@ -0,0 +1,43 @@
+package sudoku;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class UserPanel {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UserPanel window = new UserPanel();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public UserPanel() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 600, 800);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
