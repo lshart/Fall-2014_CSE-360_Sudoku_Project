af367bc1bbdad5c9c02af325905822022ce1242f
ShainaS <shainaseamster@hotmail.com>
2014-11-16 15:40:46 -0700

    Update userBoard.java
    I fixed it to display the puzzle board and such

--- src/sudoku/userBoard.java
+++ src/sudoku/userBoard.java
@@ -1,23 +1,24 @@
-package sudoku;
+package PuzzlePanel;
+
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JTextField;
-import javax.swing.JTextPane;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.*;
+import java.awt.event.*;
 
-public class userBoard extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 3012544989644192322L;
+public class userFame extends JFrame {
+
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
+	JButton btnHard, btnEasy, btnEvil, btnMedium, btnCreateNewUser; 
 
 	/**
 	 * Launch the application.
@@ -26,7 +27,7 @@ public class userBoard extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					userBoard frame = new userBoard();
+					userFame frame = new userFame();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -38,55 +39,106 @@ public class userBoard extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public userBoard() {
+	public userFame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
+		contentPane = new JPanel();// make a new panel
+		contentPane.setBackground(new Color(30, 144, 255));// set color and border and such
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnUsername = new JButton("username");
-		btnUsername.setBounds(37, 62, 89, 23);
-		contentPane.add(btnUsername);
-		
-		JButton btnPassword = new JButton("password");
-		btnPassword.setBounds(37, 143, 89, 23);
-		contentPane.add(btnPassword);
-		
-		textField = new JTextField();
-		textField.setBounds(37, 177, 86, 20);
+		textField = new JTextField();// new text fiels
+		textField.setBounds(61, 177, 86, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(37, 93, 86, 20);
+		textField_1 = new JTextField();// new text field
+		textField_1.setBounds(61, 118, 86, 20);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		
-		JButton btnEasy = new JButton("Easy");
+		JButton btnEasy = new JButton("Easy");// make a new button
 		btnEasy.setBounds(295, 49, 89, 23);
+		btnEasy.addActionListener(new ActionListener() {// if button is pushed
+			public void actionPerformed(ActionEvent e)
+			{
+				PuzzleBoard panel =new PuzzleBoard();// get new puzzle board
+				panel.setVisible(true);
+				contentPane.setVisible(false);// destroy the old content pane
+				dispose();
+			}
+		});
 		contentPane.add(btnEasy);
 		
 		JButton btnMedium = new JButton("Medium");
 		btnMedium.setBounds(295, 92, 89, 23);
+		btnMedium.addActionListener(new ActionListener() {// same as easy
+			public void actionPerformed(ActionEvent e)
+			{
+				PuzzleBoard panel =new PuzzleBoard();
+				panel.setVisible(true);
+				contentPane.setVisible(false);
+				dispose();
+			}
+		});
 		contentPane.add(btnMedium);
+		
 		
 		JButton btnHard = new JButton("Hard");
 		btnHard.setBounds(295, 131, 89, 23);
+		btnHard.addActionListener(new ActionListener() {// smae as easy
+			public void actionPerformed(ActionEvent e)
+			{
+				PuzzleBoard panel =new PuzzleBoard();
+				panel.setVisible(true);
+				contentPane.setVisible(false);
+				dispose();
+			}
+		});
 		contentPane.add(btnHard);
 		
-		JButton btnEvil = new JButton("Evil");
+		
+		JButton btnEvil = new JButton("Evil");// same as easy
 		btnEvil.setBounds(295, 176, 89, 23);
+		btnEvil.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				PuzzleBoard panel =new PuzzleBoard();
+				panel.setVisible(true);
+				contentPane.setVisible(false);
+				dispose();
+			}
+		});
 		contentPane.add(btnEvil);
 		
-		JButton btnCreateNewUser = new JButton("Create new user");
-		btnCreateNewUser.setBounds(34, 208, 113, 23);
-		contentPane.add(btnCreateNewUser);
 		
-		JTextPane txtpnPleaseEnterIn = new JTextPane();
-		txtpnPleaseEnterIn.setText("Please enter in a new user name and password");
-		txtpnPleaseEnterIn.setBounds(47, 11, 144, 40);
-		contentPane.add(txtpnPleaseEnterIn);
-	}
+		JButton btnCreateNewUser = new JButton("Create new");
+		btnCreateNewUser.setBounds(50, 208, 113, 23);
+		contentPane.add(btnCreateNewUser);
+		btnCreateNewUser.addActionListener(new ActionListener() {// if create new is clicked 
+			public void actionPerformed(ActionEvent e)// add the new user info to user manager
+			{
+							}
+		});
+		
+		JLabel lblPassword = new JLabel("Password");// make a label for the password
+		lblPassword.setBackground(new Color(30, 144, 255));
+		lblPassword.setBounds(77, 159, 70, 14);
+		contentPane.add(lblPassword);
+		
+		JLabel lblUsername = new JLabel("Username");// make a label for username
+		lblUsername.setBackground(new Color(30, 144, 255));
+		lblUsername.setBounds(77, 101, 69, 14);
+		contentPane.add(lblUsername);
+		
+		JLabel lblNewLabel = new JLabel("Sudoku");// makes the title
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNewLabel.setBackground(new Color(30, 144, 255));
+		lblNewLabel.setBounds(77, 34, 69, 14);
+		contentPane.add(lblNewLabel);
+	
+
+		}
 }
+
