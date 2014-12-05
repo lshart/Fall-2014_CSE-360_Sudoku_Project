bd318a9e2d211a0f3264f7370e15dab8078bf6af
ShainaS <shainaseamster@hotmail.com>
2014-12-04 18:14:19 -0700

    Update WinScreen.java

--- src/sudoku/WinScreen.java
+++ src/sudoku/WinScreen.java
@@ -1,3 +1,6 @@
+/////////////
+// The WinScreen class makes the win screen, it is activated once the user wins a sudoku game
+/////////////
 package sudoku;
 
 import javax.swing.JFrame;
@@ -14,11 +17,11 @@ import java.awt.Color;
 
 public class WinScreen extends JFrame 
 {
-
+	//declares global values for WinScreen class
 	private static final long serialVersionUID = -8523586303609256724L;
 	private JPanel contentPane;
 	private UserManager thisUserManager;
-
+	// default constructor makes the board,buttons, labels, and it sets the location of each piece
 	public WinScreen(UserManager thisM, int min, int sec) 
 	{
 		DecimalFormat leadingZero = new DecimalFormat("#00");//format
@@ -32,26 +35,26 @@ public class WinScreen extends JFrame
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblCongradulations = new JLabel("Congradulations!");
+		JLabel lblCongradulations = new JLabel("Congradulations!");// makes the jlabel
 		lblCongradulations.setFont(new Font("Tahoma", Font.PLAIN, 27));
 		lblCongradulations.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCongradulations.setBounds(54, 44, 210, 50);
 		contentPane.add(lblCongradulations);
 		
-		JLabel nameLabel = new JLabel(thisUserManager.getSelectedUser().getName());
+		JLabel nameLabel = new JLabel(thisUserManager.getSelectedUser().getName());// gets the current username
 		nameLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		nameLabel.setBounds(89, 105, 124, 20);
 		contentPane.add(nameLabel);
 		
 		String timeStr = min + ":" + leadingZero.format(sec);
-		JLabel timeLabel = new JLabel("Won the game in " + timeStr);
+		JLabel timeLabel = new JLabel("Won the game in " + timeStr);// returns the time
 		timeLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		timeLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		timeLabel.setBounds(77, 136, 180, 25);
 		contentPane.add(timeLabel);
 		
-		JButton btnOkay = new JButton("Okay");
+		JButton btnOkay = new JButton("Okay");// if the okay button is pressed then go back to user pannel
 		btnOkay.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent arg0) 
