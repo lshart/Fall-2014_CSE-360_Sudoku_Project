9284271a06079947f3be8719dae9ee5003790732
ShainaS <shainaseamster@hotmail.com>
2014-12-04 18:31:35 -0700

    Update User.java

--- src/sudoku/User.java
+++ src/sudoku/User.java
@@ -1,19 +1,20 @@
+/////////////
+// The User class 
+/////////////
 package sudoku;
 
 import java.io.Serializable;
 
 public class User implements Serializable
 {
-	/**
-	 * 
-	 */
+	//declares global variables for User class
 	private static final long serialVersionUID = -5220066483445795019L;
 	public static final int NUMBER_OF_DIFFICULTIES = 4;
 	private String uName;
 	private String password;
 	int[] winCount;					// should have 4 indices, initialized to zero. This is the score
 	int[] fastestTime;				// should also have 4 indices initialized to zero. This is bragging rights
-	
+	//gets the user name and password from the user panel
 	public User(String name, String pass)
 	{
 		uName = name;
@@ -28,12 +29,12 @@ public class User implements Serializable
 			fastestTime[i] = -1;
 		}
 	}
-	
+	// will start the game according to difficulty
 	public void startGame (int difficulty)
 	{
 		winCount[difficulty] --;
 	}
-	
+	// Decides if the user will win
 	public void win(int difficulty, int min, int sec, boolean underPar)
 	{
 		int time = min*60 + sec;
@@ -51,7 +52,7 @@ public class User implements Serializable
 			winCount[difficulty] ++;
 		}
 	}
-	
+	// will obtain the time	
 	public String getFastestTime(int difficulty)
 	{
 		int min = fastestTime[difficulty] / 60;
@@ -59,7 +60,7 @@ public class User implements Serializable
 		String retStr = min + ":" + sec;
 		return retStr;
 	}
-	
+	// will print out the user score
 	public String printUserScore()
 	{
 		String easyWins = "<font color=\"green\">" + winCount[0];
@@ -68,17 +69,17 @@ public class User implements Serializable
 		String evilWins = "<font color=\"black\">" +  winCount[3];
 		return easyWins + "/" + medWins + "/" + hardWins + "/" + evilWins;
 	}
-	
+	//  this method checks the password 
 	public boolean checkPass(String passToCheck)
 	{
 		return password.equals(passToCheck);
 	}
-	
+	// this will get the user name
 	public String getName()
 	{
 		return uName;
 	}
-	
+	// this will get the score according to the difficulty settings
 	public int getScore(int difficulty)
 	{
 		return winCount[difficulty];
